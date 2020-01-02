88b89c888c2a6fa537cf82a2e494a31a734fcae1
edward <edward.o2osys@gmail.com>
2020-01-02 16:18:23 +0900

    Add project files.

---
+++ SimpleServer/SimpleServerMainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SimpleServer
+{
+	partial class SimpleServerMainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ctrlLog = new System.Windows.Forms.ListBox();
+			this.groupBoxServer = new System.Windows.Forms.GroupBox();
+			this.labelServerPort = new System.Windows.Forms.Label();
+			this.labelServerIp = new System.Windows.Forms.Label();
+			this.groupBoxData = new System.Windows.Forms.GroupBox();
+			this.groupBoxSocket = new System.Windows.Forms.GroupBox();
+			this.buttonListen = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.ctrlBacklogSize = new System.Windows.Forms.NumericUpDown();
+			this.ctrlServerPort = new System.Windows.Forms.NumericUpDown();
+			this.ctrlServerIp = new System.Windows.Forms.TextBox();
+			this.groupBoxServer.SuspendLayout();
+			this.groupBoxSocket.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ctrlBacklogSize)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ctrlServerPort)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ctrlLog
+			// 
+			this.ctrlLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ctrlLog.FormattingEnabled = true;
+			this.ctrlLog.HorizontalScrollbar = true;
+			this.ctrlLog.ItemHeight = 12;
+			this.ctrlLog.Location = new System.Drawing.Point(12, 12);
+			this.ctrlLog.Name = "ctrlLog";
+			this.ctrlLog.Size = new System.Drawing.Size(514, 268);
+			this.ctrlLog.TabIndex = 0;
+			// 
+			// groupBoxServer
+			// 
+			this.groupBoxServer.Controls.Add(this.ctrlBacklogSize);
+			this.groupBoxServer.Controls.Add(this.label1);
+			this.groupBoxServer.Controls.Add(this.ctrlServerPort);
+			this.groupBoxServer.Controls.Add(this.labelServerPort);
+			this.groupBoxServer.Controls.Add(this.ctrlServerIp);
+			this.groupBoxServer.Controls.Add(this.labelServerIp);
+			this.groupBoxServer.Location = new System.Drawing.Point(12, 286);
+			this.groupBoxServer.Name = "groupBoxServer";
+			this.groupBoxServer.Size = new System.Drawing.Size(514, 52);
+			this.groupBoxServer.TabIndex = 1;
+			this.groupBoxServer.TabStop = false;
+			this.groupBoxServer.Text = "Server";
+			// 
+			// labelServerPort
+			// 
+			this.labelServerPort.AutoSize = true;
+			this.labelServerPort.Location = new System.Drawing.Point(149, 21);
+			this.labelServerPort.Name = "labelServerPort";
+			this.labelServerPort.Size = new System.Drawing.Size(31, 12);
+			this.labelServerPort.TabIndex = 0;
+			this.labelServerPort.Text = "Port:";
+			// 
+			// labelServerIp
+			// 
+			this.labelServerIp.AutoSize = true;
+			this.labelServerIp.Location = new System.Drawing.Point(7, 21);
+			this.labelServerIp.Name = "labelServerIp";
+			this.labelServerIp.Size = new System.Drawing.Size(20, 12);
+			this.labelServerIp.TabIndex = 0;
+			this.labelServerIp.Text = "IP:";
+			// 
+			// groupBoxData
+			// 
+			this.groupBoxData.Location = new System.Drawing.Point(12, 345);
+			this.groupBoxData.Name = "groupBoxData";
+			this.groupBoxData.Size = new System.Drawing.Size(514, 100);
+			this.groupBoxData.TabIndex = 2;
+			this.groupBoxData.TabStop = false;
+			this.groupBoxData.Text = "Data";
+			// 
+			// groupBoxSocket
+			// 
+			this.groupBoxSocket.Controls.Add(this.buttonListen);
+			this.groupBoxSocket.Location = new System.Drawing.Point(12, 452);
+			this.groupBoxSocket.Name = "groupBoxSocket";
+			this.groupBoxSocket.Size = new System.Drawing.Size(514, 100);
+			this.groupBoxSocket.TabIndex = 3;
+			this.groupBoxSocket.TabStop = false;
+			this.groupBoxSocket.Text = "Socket";
+			// 
+			// buttonListen
+			// 
+			this.buttonListen.Location = new System.Drawing.Point(7, 21);
+			this.buttonListen.Name = "buttonListen";
+			this.buttonListen.Size = new System.Drawing.Size(75, 23);
+			this.buttonListen.TabIndex = 4;
+			this.buttonListen.Text = "Listen";
+			this.buttonListen.UseVisualStyleBackColor = true;
+			this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(257, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Backlog Size:";
+			// 
+			// ctrlBacklogSize
+			// 
+			this.ctrlBacklogSize.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::SimpleServer.Properties.Settings.Default, "BacklogSize", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.ctrlBacklogSize.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.ctrlBacklogSize.Location = new System.Drawing.Point(346, 18);
+			this.ctrlBacklogSize.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.ctrlBacklogSize.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.ctrlBacklogSize.Name = "ctrlBacklogSize";
+			this.ctrlBacklogSize.Size = new System.Drawing.Size(65, 21);
+			this.ctrlBacklogSize.TabIndex = 3;
+			this.ctrlBacklogSize.ThousandsSeparator = true;
+			this.ctrlBacklogSize.Value = global::SimpleServer.Properties.Settings.Default.BacklogSize;
+			// 
+			// ctrlServerPort
+			// 
+			this.ctrlServerPort.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::SimpleServer.Properties.Settings.Default, "ServerPort", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.ctrlServerPort.Location = new System.Drawing.Point(186, 19);
+			this.ctrlServerPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+			this.ctrlServerPort.Name = "ctrlServerPort";
+			this.ctrlServerPort.Size = new System.Drawing.Size(65, 21);
+			this.ctrlServerPort.TabIndex = 2;
+			this.ctrlServerPort.Value = global::SimpleServer.Properties.Settings.Default.ServerPort;
+			// 
+			// ctrlServerIp
+			// 
+			this.ctrlServerIp.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SimpleServer.Properties.Settings.Default, "ServerIp", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.ctrlServerIp.Location = new System.Drawing.Point(33, 18);
+			this.ctrlServerIp.Name = "ctrlServerIp";
+			this.ctrlServerIp.Size = new System.Drawing.Size(110, 21);
+			this.ctrlServerIp.TabIndex = 1;
+			this.ctrlServerIp.Text = global::SimpleServer.Properties.Settings.Default.ServerIp;
+			// 
+			// SimpleServerMainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(538, 564);
+			this.Controls.Add(this.groupBoxSocket);
+			this.Controls.Add(this.groupBoxData);
+			this.Controls.Add(this.groupBoxServer);
+			this.Controls.Add(this.ctrlLog);
+			this.Name = "SimpleServerMainForm";
+			this.Text = "SImpleServerMainForm";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SimpleServerMainForm_FormClosing);
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SimpleServerMainForm_FormClosed);
+			this.groupBoxServer.ResumeLayout(false);
+			this.groupBoxServer.PerformLayout();
+			this.groupBoxSocket.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.ctrlBacklogSize)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ctrlServerPort)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox ctrlLog;
+		private System.Windows.Forms.GroupBox groupBoxServer;
+		private System.Windows.Forms.Label labelServerIp;
+		private System.Windows.Forms.TextBox ctrlServerIp;
+		private System.Windows.Forms.NumericUpDown ctrlServerPort;
+		private System.Windows.Forms.Label labelServerPort;
+		private System.Windows.Forms.GroupBox groupBoxData;
+		private System.Windows.Forms.GroupBox groupBoxSocket;
+		private System.Windows.Forms.Button buttonListen;
+		private System.Windows.Forms.NumericUpDown ctrlBacklogSize;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ SimpleServer/SimpleServerMainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimpleServer
+{
+	public partial class SimpleServerMainForm : Form
+	{
+		readonly int MAX_LOG_ROWS = 4096;
+
+		Socket socket;
+		Socket clientSocket;
+		SocketAsyncEventArgs saea;
+		CancellationTokenSource cts;
+		AutoResetEvent listenEvent;
+		Thread listenThread;
+
+		public SimpleServerMainForm()
+		{
+			InitializeComponent();
+		}
+
+		void AddLog(string log)
+		{
+			if (this.ctrlLog.InvokeRequired)
+			{
+				// UI 쓰레드가 아닌 쓰레드에서 호출했을 때: UI 쓰레드에게 처리해 달라고 요청
+				this.ctrlLog.Invoke(new Action(() => AddLog(log)));
+			}
+			else
+			{
+				// UI 쓰레드라면: 최대 로그 갯수 유지하면서 로그 추가
+				if (MAX_LOG_ROWS < this.ctrlLog.Items.Count)
+				{
+					this.ctrlLog.Items.RemoveAt(0);
+				}
+				this.ctrlLog.Items.Add(log);
+			}
+		}
+
+		private void SimpleServerMainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			this.cts.Cancel();
+			this.listenEvent.Set();
+		}
+
+		private void SimpleServerMainForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Properties.Settings.Default.Save();
+		}
+
+		private void buttonListen_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				this.socket.Bind(new IPEndPoint(IPAddress.Parse(Properties.Settings.Default.ServerIp), Properties.Settings.Default.ServerPort));
+				this.socket.Listen(Properties.Settings.Default.BacklogSize);
+				this.saea = new SocketAsyncEventArgs();
+				this.saea.Completed += new EventHandler<SocketAsyncEventArgs>(AcceptCompleted);
+				this.listenEvent = new AutoResetEvent(false);
+				this.cts = new CancellationTokenSource();
+				this.listenThread = new Thread(() => RepeatAccept(this.socket, this.saea, this.cts.Token));
+				this.listenThread.Start();
+			}
+			catch (Exception ex)
+			{
+				AddLog($"{MethodBase.GetCurrentMethod().Name}, {ex.GetType().Name}, {ex.Message}");
+			}
+		}
+
+		/// <summary>
+		/// AcceptAsync를 반복하기 위한 래퍼 메서드
+		/// </summary>
+		/// <remarks>
+		/// - AcceptAsync는 비동기 또는 동기로 완료될 수 있음
+		/// - 비동기로 완료되면, 콜백 메서드가 자동으로 호출되며, 이때 다시 AcceptAsync를 호출하면 무한 반복할 수 있으므로 문제 없음
+		/// - 동기로 완료되면, 콜백 메서드가 호출되지 않으며, 수동으로 AcceptAsync를 호출해야 함. 따라서 콜백 메서드만으로 무한 반복할 수 없음
+		/// - 동기 완료를 처리하기 위해 비동기/동기 완료 모두 처리할 수 있도록, 루프를 돌면서 AcceptAsync 호출해서, 비동기 완료는 콜백 메서드가, 동기 완료는 이 메서드에서 처리
+		/// </remarks>
+		/// <param name="socket"></param>
+		/// <param name="saea"></param>
+		/// <param name="ct"></param>
+		void RepeatAccept(Socket socket, SocketAsyncEventArgs saea, CancellationToken ct)
+		{
+			AddLog($"{MethodBase.GetCurrentMethod().Name}");
+
+			while (!ct.IsCancellationRequested)
+			{
+				// SocketAsyncEventArgs 재활용
+				saea.AcceptSocket = null;
+
+				bool pending = true;
+				try
+				{
+					pending = socket.AcceptAsync(saea);
+					AddLog($"{MethodBase.GetCurrentMethod().Name}, AcceptAsync()");
+				}
+				catch (Exception ex)
+				{
+					AddLog($"{MethodBase.GetCurrentMethod().Name}, {ex.GetType().Name}, {ex.Message}");
+					continue;
+				}
+
+				// 동기 완료됐다면 콜백 메서드가 자동으로 호출되지 않으므로 직접 호출해야 함
+				if (!pending)
+				{
+					AcceptCompleted(this, saea);
+				}
+
+				// AcceptAsync는 동기/비동기 상관없이 즉시 반환하므로 while 루프가 계속 실행되는 문제가 있음
+				// 이런 상황을 방지하기 위해 오토리셋이벤트를 기다리는 코드를 추가
+				// 오토리셋이벤트는 AcceptCompleted에서 셋
+				this.listenEvent.WaitOne();
+			}
+		}
+
+		void AcceptCompleted(object sender, SocketAsyncEventArgs e)
+		{
+			if (e.SocketError == SocketError.Success)
+			{
+				this.clientSocket = e.AcceptSocket;
+				AddLog($"{MethodBase.GetCurrentMethod().Name}, ClientSocket={e.AcceptSocket.Handle}");
+			}
+			else
+			{
+				AddLog($"{MethodBase.GetCurrentMethod().Name}, {e.SocketError.ToString()}");
+			}
+
+			this.listenEvent.Set();
+		}
+	}
+}
